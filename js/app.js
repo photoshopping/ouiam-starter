4567dea6657fdf4d069066c248c4c5f775b52676
seo4dev <seofordev@gmail.com>
2023-06-07 15:55:06 +0100

    fixing salutation js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
-var greeting = document.getElementById("greeting");
-var d = new Date();
-var time = d.getHours();
-
-if (time < 14) {
-  greeting.innerHTML = "Bonjour!";
-  console.log("bonjour");
-}
-if (time > 14) {
-  greeting.innerHTML = "Bonsoir!";
-  console.log("bonsoir");
-}
-
 // Google Tag Manager
 
 (function (w, d, s, l, i) {
