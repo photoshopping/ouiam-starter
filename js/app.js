3042a85439e16bba3651447f85d6243c251aaf3b
seo4dev <seofordev@gmail.com>
2023-06-07 13:57:51 +0100

    defering tag manager

--- js/app.js
+++ js/app.js
@@ -10,3 +10,18 @@ if (time > 14) {
   greeting.innerHTML = "Bonsoir!";
   console.log("bonsoir");
 }
+
+// Google Tag Manager
+
+(function (w, d, s, l, i) {
+  w[l] = w[l] || [];
+  w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+  var f = d.getElementsByTagName(s)[0],
+    j = d.createElement(s),
+    dl = l != "dataLayer" ? "&l=" + l : "";
+  j.async = true;
+  j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+  f.parentNode.insertBefore(j, f);
+})(window, document, "script", "dataLayer", "GTM-KH7LPK3");
+
+//End Google Tag Manager
