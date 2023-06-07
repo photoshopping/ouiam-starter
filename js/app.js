e61712f13b46d085dd48d0f6a62060adc5ef379e
seo4dev <seofordev@gmail.com>
2023-06-07 13:06:07 +0100

    remove monster icon

--- js/app.js
+++ js/app.js
@@ -2,9 +2,6 @@ var greeting = document.getElementById("greeting");
 var d = new Date();
 var time = d.getHours();
 
-greeting.style.color = "white";
-greeting.style.fontWeight = "bold";
-
 if (time < 14) {
   greeting.innerHTML = "Bonjour!";
   console.log("bonjour");
